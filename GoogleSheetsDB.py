9b61d799ed87f967931086bbcaa9c75635866618
Rajat Monga <rajatmonga29@gmail.com>
2018-03-04 14:11:06 -0600

    Update GoogleSheetsDB.py

--- GoogleSheetsDB.py
+++ GoogleSheetsDB.py
@@ -1,8 +1,11 @@
-import grspread 
+import grspread
+from oauth2client.service_account import ServiceAccountCredentials
+ 
+
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
 
 
-scope = ['']
-creds = ''
-client = ''
-
-
+workable_sheet = client.open('').sheet1
+list_of_maps = workable_sheet.get_all_records()
