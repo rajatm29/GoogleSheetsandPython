c41f12e44499aeb313a694cf37b6fff945ba42b9
Rajat Monga <rajatmonga29@gmail.com>
2018-03-06 20:15:42 -0600

    Update GoogleSheetsDB.py

--- GoogleSheetsDB.py
+++ GoogleSheetsDB.py
@@ -7,5 +7,11 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', s
 client = gspread.authorize(creds)
 
 
-workable_sheet = client.open('').sheet1
+workable_sheet = client.open('Name of Sheet').sheet1
 list_of_maps = workable_sheet.get_all_records()
+
+#inserting a row in workable_sheet using python 
+row = ["Inserting", "a", "record", "using", "Python"]
+index = 1
+workable_sheet.insert_row(row, index)
+
